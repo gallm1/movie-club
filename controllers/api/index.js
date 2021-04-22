633e782df846ff99097ebab9a79da99a92f96db4
Cesar Campos <cesarorlandocampos@gmail.com>
2021-04-22 11:18:31 -0500

    Save before denis pull from main

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,7 +3,7 @@ const userRoutes = require('./userRoutes');
 const reviewsRoutes = require('./reviewsRoutes');
 const moviesRoutes = require('./moviesRoutes');
 
-// router.use('/users', userRoutes);
+router.use('/users', userRoutes);
 // router.use('/reviews', reviewsRoutes);
 router.use('/movies', moviesRoutes);
 
--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-//const { movies, User } = require('../models');
+const { Review, User } = require('../models');
 //const withAuth = require('../utils/auth');
 
 // require models

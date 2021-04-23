95726339978bb661240dd159a0fa7e10977883a6
denis salvino <denis.salvino23@gmail.com>
2021-04-23 00:29:59 -0500

    working handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -18,7 +18,7 @@ async function getMoviesPoster(posterURL) {
 //ICEBOX: be able to sort movies
 //this is grabs the data and posters of movies
 async function getMoviesAll() {
-    let moviesURL = 'https://api.themoviedb.org/3/discover/movie?api_key=' + process.env.TMDB_API + '&language=en-US&sort_by=popularity.desc&page=1'
+    let moviesURL = 'https://api.themoviedb.org/3/discover/movie?api_key=' + process.env.API_KEY + '&language=en-US&sort_by=popularity.desc&page=1'
     try {
         const moviesAll = await axios.get(moviesURL);
         // getMoviesDetails(moviesAll.data.results);
@@ -36,7 +36,7 @@ router.get('/', async (req, res) => {
 
 //create another get for an individual movie, or selected movie 
 async function oneMovie(MovieID) {
-    let selectedMovie = 'https://api.themoviedb.org/3/movie/' + MovieID + '?api_key=' + process.env.TMDB_API + '&language=en-US&'
+    let selectedMovie = 'https://api.themoviedb.org/3/movie/' + MovieID + '?api_key=' + process.env.API_KEY + '&language=en-US&'
     try {
         const moviesData = await axios.get(selectedMovie);
         // console.log{ id, title, overview, poster_path, release_date, tagline, vote_average };
@@ -54,10 +54,10 @@ router.get('/movie/:id', async (req, res) => {
 
 //iterates through 500 pages to select a random movie
 async function weeklyMovie() {
-    let randomMovie = 'https://api.themoviedb.org/3/discover/movie?api_key=' + process.env.TMDB_API + '&language=en-US&sort_by=popularity.desc&page=' + randomPageNumber(500);
+    let randomMovie = 'https://api.themoviedb.org/3/discover/movie?api_key=' + process.env.API_KEY + '&language=en-US&sort_by=popularity.desc&page=' + randomPageNumber(500);
     try {
         const pageObj = await axios.get(randomMovie);
-        console.log(pageObj.data.results[randomPageNumber(pageObj.data.results.length)]);
+        // console.log(pageObj.data.results[randomPageNumber(pageObj.data.results.length)]);
         return 'reviews', pageObj.data.results[randomPageNumber(pageObj.data.results.length)]
     } catch (err) {
         console.error(err);
@@ -69,22 +69,28 @@ function randomPageNumber(max) {
     // console.log(randomPageInt);
     return randomPageInt
 }
-//queries user search to search for movie 
+// queries user search to search for movie 
 async function searchMovieQuery(userSearch) {
-    // let userSearch = process.argv[2];
-    try {
-        let moviesSearchURL = await axios.get('https://api.themoviedb.org/3/search/movie?api_key=' + process.env.TMDB_API + '&language=en-US&query=' + userSearch + '&page=1&include_adult=false')
-        console.log(moviesSearchURL.data.results);
-        return moviesSearchURL.data.results
-    } catch (err) {
-        console.error(err);
+        // let userSearch = process.argv[2];
+        try {
+            let moviesSearchURL = await axios.get('https://api.themoviedb.org/3/search/movie?api_key=' + process.env.API_KEY + '&language=en-US&query=' + userSearch + '&page=1&include_adult=false')
+            // console.log(moviesSearchURL.data.results);
+            // console.log(userSearch);
+            return moviesSearchURL.data.results
+        } catch (err) {
+            console.error(err);
+        }
     }
-}
 
-router.get('/search:id', async (req, res) => {
-    const retrievedMoviesFromSearch = await searchMovieQuery(req.params.id);
-    res.render('search', retrievedMoviesFromSearch);
-});
+router.get('/search:query', async (req, res) => {
+        const retrievedMoviesFromSearch = await searchMovieQuery(req.query.query);
+        res.render('search', retrievedMoviesFromSearch);
+    });
 
 module.exports = router;
+
+
+
+
+
 
